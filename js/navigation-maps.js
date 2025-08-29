9c53f9b0f373086690cc4411c10a3afe84c68197
ashinoff <ashinoff@gmail.com>
2025-08-29 14:25:42 +0300

    Create navigation-maps.js

---
+++ js/navigation-maps.js
@@ -0,0 +1,26 @@
+const navigationMaps = {
+    'r057-2022': {
+        docUrl: '/regulations/r057-2022/source.docx',
+        sections: {
+            'normative': '1',
+            'general': '2',
+            'info-activities': '3',
+            'parallel-connection': '4.3',
+            'no-contract-consumption': '4.4',
+            'restriction-control': '4.5',
+            'planning-detection': '4.6',
+            'consumption-analysis': '4.7',
+            'check-preparation': '4.8',
+            'conducting-checks': '4.9',
+            'calculation-unaccounted': '4.10',
+            'calculation-unaccounted-phys': '4.11',
+            'contractless-calculation': '4.12',
+            'meter-malfunction': '4.13',
+            'commission-review': '4.14',
+            'damage-compensation': '4.15',
+            'cost-recovery': '4.16',
+            'law-enforcement': '4.18',
+            'operational-control': '4.19'
+        }
+    }
+};
